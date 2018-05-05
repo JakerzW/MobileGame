134750eae016b5f384d868cd7b02c41b9eef8264
Jake Walder <jakerzw@hotmail.co.uk>
2018-05-05 17:54:41 +0100

    Title working

---
+++ Mobile Game/Assets/Scripts/TitleController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour {
+
+    private AssetBundle Scenes;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Scenes = AssetBundle.LoadFromFile("Assets/Scenes");
+        	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayGame()
+    {
+        Debug.Log("Loading Scene...");
+        SceneManager.LoadScene("Main Scene");
+    }
+}
